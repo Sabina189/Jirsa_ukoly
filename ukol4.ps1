36991de91237c1019cf46b6c1e5950d43ea664ed
Sabina Kubešová <135815715+Sabina189@users.noreply.github.com>
2025-11-06 12:07:07 +0100

    ukol4.ps1

--- ukol4.ps1
+++ ukol4.ps1
@@ -1,44 +1,22 @@
-# Kontrola a nastavení NumLock pro přihlášení 
+Get-ItemProperty -Path 'Registry::HKEY_USERS\.DEFAULT\Control Panel\Keyboard' -Name 'InitialKeyboardIndicators'
+Set-ItemProperty -Path 'Registry::HKEY_USERS\.DEFAULT\Control Panel\Keyboard' -Name 'InitialKeyboardIndicators' -Value '2'
 
-$numLockRegPath = "HKU\.DEFAULT\Control Panel\Keyboard"
-$numLockValueName = "InitialKeyboardIndicators"
+New-Item -Path 'HKCU:\Hrátky s PowerShellem' -Force
+$user = $env:USERNAME
+$user
 
-$currentValue = (Get-ItemProperty -Path $numLockRegPath -Name $numLockValueName).$numLockValueName
+$computer = $env:COMPUTERNAME
+$computer
 
-Write-Host "Aktuální hodnota NumLock při přihlášení: $currentValue"
+$date = (Get-Date).ToString('yyyy-MM-dd HH:mm:ss')
+$date
 
-if ($currentValue -ne "2") {
-    Set-ItemProperty -Path $numLockRegPath -Name $numLockValueName -Value "2"
-    Write-Host "NumLock nebyl zapnut. Hodnota byla nastavena na 2 (zapnuto)."
-} else {
-    Write-Host "NumLock je již zapnut při přihlášení."
-}
+$psver = $PSVersionTable.PSVersion.ToString()
+$psver
 
-# Vytvoření klíče Hrátky s PowerShellem a uložení informací 
+New-ItemProperty -Path 'HKCU:\Hrátky s PowerShellem' -Name 'UserName'      -PropertyType String -Value $user   -Force
+New-ItemProperty -Path 'HKCU:\Hrátky s PowerShellem' -Name 'ComputerName'  -PropertyType String -Value $computer -Force
+New-ItemProperty -Path 'HKCU:\Hrátky s PowerShellem' -Name 'DateSaved'     -PropertyType String -Value $date   -Force
+New-ItemProperty -Path 'HKCU:\Hrátky s PowerShellem' -Name 'PowerShellVer' -PropertyType String -Value $psver  -Force
 
-$regPath = "HKCU:\Hrátky s PowerShellem"
-
-if (-not (Test-Path $regPath)) {
-    New-Item -Path $regPath | Out-Null
-    Write-Host "Klíč '$regPath' byl vytvořen."
-}
-
-$userName = $env:USERNAME
-$computerName = $env:COMPUTERNAME
-$currentDate = Get-Date -Format "dd.MM.yyyy HH:mm:ss"
-$psVersion = $PSVersionTable.PSVersion.ToString()
-
-New-ItemProperty -Path $regPath -Name "Uživatel" -Value $userName -PropertyType String -Force | Out-Null
-New-ItemProperty -Path $regPath -Name "Počítač" -Value $computerName -PropertyType String -Force | Out-Null
-New-ItemProperty -Path $regPath -Name "Datum" -Value $currentDate -PropertyType String -Force | Out-Null
-New-ItemProperty -Path $regPath -Name "Verze PowerShellu" -Value $psVersion -PropertyType String -Force | Out-Null
-
-# Výpis potvrzení 
-
-Write-Host "`n--- Uložené informace ---"
-Write-Host "Uživatel: $userName"
-Write-Host "Počítač: $computerName"
-Write-Host "Datum: $currentDate"
-Write-Host "Verze PowerShellu: $psVersion"
-Write-Host "------------------------------------"
-Write-Host "Všechny údaje byly úspěšně zapsány do registru: $regPath"
+Get-ItemProperty -Path 'HKCU:\Hrátky s PowerShellem' | Format-List
